2ac134b77a09147d7813a17cf6a660296734635c
KovalevichAliaksandr <servistest@gmail.com>
2017-02-22 10:55:16 +0300

    configured application context for dao tests.

---
+++ test-db/src/main/resources/add-test-data.sql
@@ -0,0 +1,6 @@
+insert INTO department(id,name_department) VALUES (1,'credit');
+insert INTO department(id,name_department) VALUES (2,'monetary');
+
+INSERT INTO employee (id,first_name, last_name, dob, salary, id_department) VALUES (1,'Scott','Wolf','1944-05-18',5000,1);
+INSERT INTO employee (id,first_name, last_name, dob, salary, id_department) VALUES (2,'Henry','Jackson','1974-02-11',3000,1);
+COMMIT ;
